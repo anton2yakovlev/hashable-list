57aad326e9d9e7b28d4326b93babefd82f1dc0b6
Anton Yakovlev <Kolomin58@gmail.com>
2024-06-21 16:55:36 +0300

    add __hash__ method

--- hashable_list/hashable_list.py
+++ hashable_list/hashable_list.py
@@ -1,3 +1,10 @@
+from uuid import uuid4, UUID
+
+
 class HashableList(list):
     def __init__(self, *args):
         super().__init__(args)
+        self.hash_key = int(uuid4())
+
+    def __hash__(self) -> UUID:
+        return self.hash_key
