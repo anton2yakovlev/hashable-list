64598eb88890f661ea49cf1feadebe9ffd5ed9db
Anton Yakovlev <Kolomin58@gmail.com>
2024-06-21 18:20:03 +0300

    add more tests

--- tests/test_hashable_list.py
+++ tests/test_hashable_list.py
@@ -25,3 +25,29 @@ def test_add_value_to_dict_key():
 def test_it_has_hash():
     hashable_list = HashableList()
     assert hash(hashable_list) is not None
+
+
+def test_contains():
+    hashable_list = HashableList(1, 2, 3)
+    assert 1 in hashable_list
+
+
+def test_len():
+    hashable_list = HashableList(1, 2, 3, 4, 5)
+    assert len(hashable_list) == 5
+
+
+def test_multitypes():
+    hashable_list = HashableList(1, "2", [3, 4], {5: 6}, 7.0)
+    assert hashable_list
+
+
+def test_append():
+    hashable_list = HashableList(1, 2)
+    hashable_list.append(3)
+    assert hashable_list == [1, 2, 3]
+
+
+def test_pop():
+    hashable_list = HashableList(1, 2, 3)
+    assert hashable_list.pop() == 3
