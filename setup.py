a76351938b590883697bf824222181d1e9655081
Anton Yakovlev <Kolomin58@gmail.com>
2024-06-21 18:57:13 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hashable-list",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A simple example library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/anton2yakovlev/hashable-list",
+    author="Anton Yakovlev",
+    author_email="anton2yakovlev@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.9",
+)
