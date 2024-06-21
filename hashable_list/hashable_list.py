b9afd0013805d04c4e1fba88a5a5706ab6540a4b
Anton Yakovlev <Kolomin58@gmail.com>
2024-06-21 18:51:01 +0300

    fix mypy issues

--- hashable_list/hashable_list.py
+++ hashable_list/hashable_list.py
@@ -1,22 +1,20 @@
 from uuid import uuid4
-from typing import Any, Iterable, List, Iterator, Union, overload
+from typing import Any, Callable, Iterable, List, Iterator, Union, overload, Optional
 
 
 class HashableList:
-    @overload
-    def __init__(self) -> None: ...
-    @overload
-    def __init__(self, iterable: Iterable[Any]) -> None: ...
-
     def __init__(self, *args: Union[Any, Iterable[Any]]) -> None:
+        items: Optional[List[Any]] = None
         if (
             len(args) == 1
             and isinstance(args[0], Iterable)
             and not isinstance(args[0], (str, bytes, bytearray))
         ):
-            self.items: List[Any] = list(args[0])
+            items = list(args[0])
         else:
-            self.items: List[Any] = list(args)
+            items = list(args)
+
+        self.items = items
         self.hash_key = int(uuid4())
 
     def __hash__(self) -> int:
@@ -50,7 +48,7 @@ class HashableList:
     def append(self, value: Any) -> None:
         self.items.append(value)
 
-    def extend(self, values: List[Any]) -> None:
+    def extend(self, values: Iterable[Any]) -> None:
         self.items.extend(values)
 
     def insert(self, index: int, value: Any) -> None:
@@ -65,20 +63,24 @@ class HashableList:
     def clear(self) -> None:
         self.items.clear()
 
-    def index(self, value: Any, start: int = 0, end: int = None) -> int:
+    def index(self, value: Any, start: int = 0, end: Optional[int] = None) -> int:
+        if end is None:
+            end = len(self.items)
         return self.items.index(value, start, end)
 
     def count(self, value: Any) -> int:
         return self.items.count(value)
 
-    def sort(self, *, key: Any = None, reverse: bool = False) -> None:
+    def sort(
+        self, *, key: Optional[Callable[[Any], Any]] = None, reverse: bool = False
+    ) -> None:
         self.items.sort(key=key, reverse=reverse)
 
     def reverse(self) -> None:
         self.items.reverse()
 
     def copy(self) -> "HashableList":
-        new_copy = HashableList(*self.items)
+        new_copy = HashableList(self.items)
         new_copy.hash_key = self.hash_key
         return new_copy
 
