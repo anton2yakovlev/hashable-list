aa65e9d9e7eb6f596e4888e3e7eefd9660188ebd
Anton Yakovlev <Kolomin58@gmail.com>
2024-06-21 20:03:03 +0300

    update library version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hashable-list",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     description="A hashable list type in Python",
     long_description=open("README.md").read(),
